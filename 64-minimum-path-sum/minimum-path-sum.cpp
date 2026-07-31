090b618434dd8a01629484b4187e13b78ee5a7d7
Swati Raghav <swatiraghav771@gmail.com>
2026-07-31 23:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (96.28%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+            int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue; // starting cell, no change
+                } else if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];       // only from left
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];       // only from above
+                } else {
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
